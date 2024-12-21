16a7c88a7cfb929689a8e713dd459c4eadd13330
Dhruti HIrapara <hiraparadhruti26@gmail.com>
2024-12-21 17:04:44 +0530

    gh

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 const About = () => {
   return (
     <>
+    {/* ljjk */}
       <div className="flex flex-col lg:flex-row w-full lg:mt-12 md:mt-20 lg:px-40 px-4">
         {/* Left part */}
         <div className="w-full lg:w-1/2">
